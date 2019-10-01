13e3d95f09699eb1b0821541dfd0ec45bcb972b0
tjclawson <tjamesclawson@gmail.com>
2019-10-01 18:01:57 -0400

    add okhttp and stetho

--- M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/Retrofit.kt
+++ M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/Retrofit.kt
@@ -1,11 +1,14 @@
 package com.lambdaschool.sprintchallenge3_pokeapi
 
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.concurrent.TimeUnit
 
 interface PokemonApi{
 
@@ -17,11 +20,18 @@ interface PokemonApi{
         companion object {
             val BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
             val gson = Gson()
+            val okHttpClient = OkHttpClient.Builder()
+                    .addNetworkInterceptor(StethoInterceptor())
+                    .retryOnConnectionFailure(false)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .build()
 
             fun create(): PokemonApi {
 
                 return Retrofit.Builder()
                         .baseUrl(PokemonApi.Factory.BASE_URL)
+                        .client(okHttpClient)
                         .addConverterFactory(GsonConverterFactory.create(gson))
                         .build()
                         .create(PokemonApi::class.java)
